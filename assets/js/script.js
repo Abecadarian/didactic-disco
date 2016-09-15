4e121b9c1919145d7926655a1df18b12be42b68b
Michael Neild <jeremyn@Jeremys-MacBook-Pro.local>
2016-09-15 17:09:24 +1200

    Add project files and folders

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(function () {
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+        $('#get-weather-btn').click(getWeatherWithZipCode);
+        getWeatherWithGeoLocation();
+    }
+    ;
+});
