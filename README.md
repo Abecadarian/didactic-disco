deece85088cdfb39ec409c1d3b9b1ad8f7b9e229
Michael Neild <jeremyn@Jeremys-MacBook-Pro.local>
2016-09-15 17:26:37 +1200

    New upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# didactic-disco
-Weather app for MSA submission
---
+++ assets/js/helloPerson.ts
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Deesp fdafsjdfsjakfjk;ksSea";
+document.body.innerHTML = greeter(user);
